0816bd9df011546579fa1989d9ecce6f13f15be4
Rakibul Yeasin <rytotul@yahoo.com>
2020-03-04 20:31:53 +0000

    :fire:

--- main.py
+++ main.py
@@ -20,27 +20,30 @@ all_data = ["Name", "Phone", "Website", "Address"]
 
 sheet_data = []
 
-for i in range(1, 31):
-  page = rq.get(f"{site}{i}")
-  bs = BeautifulSoup(page.text, "html.parser")
-  name = bs.find_all('a', {"class": "business-name"})
-  phone = bs.find_all('div', {"class": "phones"})
-  addr1 = bs.find_all('div', {"class": "street-address"})
-  addr2 = bs.find_all('div', {"class": "locality"})
-  wb = bs.find_all('a', {"class": "track-visit-website"})
+try:
+  for i in range(1, 100):
+    page = rq.get(f"{site}{i}")
+    bs = BeautifulSoup(page.text, "html.parser")
+    name = bs.find_all('a', {"class": "business-name"})
+    phone = bs.find_all('div', {"class": "phones"})
+    addr1 = bs.find_all('div', {"class": "street-address"})
+    addr2 = bs.find_all('div', {"class": "locality"})
+    wb = bs.find_all('a', {"class": "track-visit-website"})
 
-  for n, p, a1, a2, lnk in zip(name, phone, addr1, addr2, wb):
-    # names.write(n.find('span').text + "\n")
-    # phones.write(p.text + "\n")
-    # address.write(f"{a1.text}, {a2.text}\n")
-    # web.write(lnk["href"] + "\n")
-    dct = {
-      "Name": n.find('span').text,
-      "Phone": p.text,
-      "Website": lnk["href"],
-      "Address": f"{a1.text}, {a2.text}"
-    }
-    sheet_data.append(dct)
+    for n, p, a1, a2, lnk in zip(name, phone, addr1, addr2, wb):
+      # names.write(n.find('span').text + "\n")
+      # phones.write(p.text + "\n")
+      # address.write(f"{a1.text}, {a2.text}\n")
+      # web.write(lnk["href"] + "\n")
+      dct = {
+        "Name": n.find('span').text,
+        "Phone": p.text,
+        "Website": lnk["href"],
+        "Address": f"{a1.text}, {a2.text}"
+      }
+      sheet_data.append(dct)
+except:
+  print("Limit reached")
 
 with open("data.csv", "w") as csv_data:
   sheet = csv.DictWriter(csv_data, restval=",", fieldnames=all_data)
